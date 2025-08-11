11d5bfa5a76e3d3a6f52145d0334fa536a896a67
David Nduonofit <nduonofitdavidfortune@gmail.com>
2025-08-11 15:05:02 +0100

    feature: implement about us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Home from './pages/Home'
 import './App.css'
+import About from './components/About'
 
 function App() {
 
   return (
     <>
       <Home />
+      <About />
     </>
   )
 }
